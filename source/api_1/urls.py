5e9f978118c10933d13e017ad1ec44280768a565
Alexandr <nuvorish81@mail.ru>
2020-10-17 19:30:50 +0600

    В api_v1 добавленна DetailView - выводит данные одной статьи.

--- source/api_1/urls.py
+++ source/api_1/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
+from api_1.views import get_token_view, ArticleCreateView, ArticleListView, ArticleDetailView
+
 app_name = 'api_v1'
 
 urlpatterns = [
+    path('get_token/', get_token_view, name='get_token'),
+    path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('article/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_view'),
 ]
--- source/api_1/views.py
+++ source/api_1/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import json
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views.generic import View
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+from api_1.serializers import ArticleSerializer
+from webapp.models import Article
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+class ArticleListView(View):
+    def get(self, request, *args, **kwargs):
+        objects = Article.objects.all()
+        slr = ArticleSerializer(objects, many=True)
+        return JsonResponse(slr.data, safe=False)
+
+
+class ArticleDetailView(View):
+    def get(self, request, *args, **kwargs):
+        print(self.kwargs.get('pk'))
+        article = get_object_or_404(Article, pk=self.kwargs.get('pk'))
+        srl = ArticleSerializer(article)
+        return JsonResponse(srl.data)
+
+
+class ArticleCreateView(View):
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        slr = ArticleSerializer(data=data)
+        if slr.is_valid():
+            article = slr.save()
+            return JsonResponse(slr.data, safe=False)
+        else:
+            response = JsonResponse(slr.errors, safe=False)
+            response.status_code = 400
+            return response
